2ba01b2c9d3aa0d916b260ee19c36c89cd387c35
Andrew Salo <andrew.salo@gmail.com>
2020-12-27 21:13:59 +0300

    Hex cell styles implemented

--- src/view/map.rs
+++ src/view/map.rs
@@ -6,6 +6,7 @@ use std::{
 use tui::{
     buffer::Buffer,
     layout::Rect,
+    style::{Color, Style},
     widgets::{Block, Borders, StatefulWidget, Widget},
 };
 
@@ -13,13 +14,20 @@ const MAX_SCALE: u16 = 5;
 const MIN_SCALE: u16 = 1;
 
 pub struct MapState {
+    styles: Vec<(char, Style)>,
     scale: u16,
     map: Rc<Map>,
 }
 
 impl MapState {
     pub fn new(map: &Rc<Map>) -> Self {
+        let styles = vec![
+            ('0', Style::default().fg(Color::Rgb(20, 20, 20))),
+            ('1', Style::default().fg(Color::Blue)),
+            ('2', Style::default().fg(Color::Yellow))
+        ];
         Self {
+            styles,
             scale: MIN_SCALE,
             map: Rc::clone(map),
         }
@@ -53,42 +61,52 @@ impl<'a> StatefulWidget for MapWidget<'a> {
         let inner = self.block.inner(area);
         self.block.render(area, buf);
 
-        let mut render_hex = |pos_i: u16, pos_j: u16| {
+        let mut render_hex = |pos_i: u16, pos_j: u16, style: &Style| {
             let start_x = 3 * pos_j * state.scale;
             let start_y = 2 * pos_i * state.scale + (pos_j % 2) * state.scale;
 
-            let mut render_char = |x: u16, y: u16, ch: char| {
+            let mut render_char = |x: u16, y: u16, ch: char, style: &Style| {
                 let x = start_x + x;
                 let y = start_y + y;
                 if x < inner.width && y < inner.height {
-                    buf.get_mut(x + inner.x, y + inner.y).set_char(ch);
+                    let cell = buf.get_mut(x + inner.x, y + inner.y);
+                    cell.set_style(*style);
+                    cell.set_char(ch);
+                    //buf.get_mut(x + inner.x, y + inner.y).set_char(ch);
                 }
             };
 
             for i in 0..state.scale {
                 // N
-                render_char(state.scale + i, 0, '_');
-                render_char(2 * state.scale + i, 0, '_');
+                render_char(state.scale + i, 0, '_', style);
+                render_char(2 * state.scale + i, 0, '_', style);
                 // NW
-                render_char(i, state.scale - i, '╱');
+                render_char(i, state.scale - i, '╱', style);
                 // NE
-                render_char(3 * state.scale + i, i + 1, '╲');
+                render_char(3 * state.scale + i, i + 1, '╲', style);
                 // SW
-                render_char(i, state.scale + i + 1, '╲');
+                render_char(i, state.scale + i + 1, '╲', style);
                 // SE
-                render_char(3 * state.scale + i, 2 * state.scale - i, '╱');
+                render_char(3 * state.scale + i, 2 * state.scale - i, '╱', style);
                 // S
-                render_char(state.scale + i, 2 * state.scale, '_');
-                render_char(2 * state.scale + i, 2 * state.scale, '_');
+                render_char(state.scale + i, 2 * state.scale, '_', style);
+                render_char(2 * state.scale + i, 2 * state.scale, '_', style);
             }
         };
 
         let grid = &(*state.map).grid;
 
-        for (i, row) in grid.iter().enumerate() {
-            for (j, ch) in row.chars().enumerate() {
-                if ch != ' ' {
-                    render_hex(i as u16, j as u16);
+        // TODO: rework grid traversal
+        // Cell styles have priorities. Curent implementation traverses
+        // whole grid for each cell style, so that cells with higher priority
+        // styles will override ones with lower priority.
+        // Complexity: num_of_cell_styles * grid_width * grid_height
+        for (ch_type, style) in &state.styles {
+            for (i, row) in grid.iter().enumerate() {
+                for (j, ch) in row.chars().enumerate() {
+                    if ch == *ch_type {
+                        render_hex(i as u16, j as u16, style);
+                    }
                 }
             }
         }
